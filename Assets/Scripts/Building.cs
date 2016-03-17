0bae2403bc80ee213b7e669af1cea01915cb2dfe
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2016-03-17 10:37:07 +0100

    Map verplaatsing en building klasse

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+    public BuildingType type;
+    public int health;
+    public int damage;
+    public int armor;
+    public int[] buildingCost;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -3,3 +3,5 @@ using System.Collections;
 
 public enum ResourceType { Gold, Lumber, Mana }
 
+public enum BuildingType { Attack, Defense, Harvester }
+
